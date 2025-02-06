cdf2460471a31fe5f661653d83d9e2a797972a8b
etteifot <mudohs111@gmail.com>
2025-02-06 10:15:57 +0000

    Update Githubfile.js

--- Githubfile.js
+++ Githubfile.js
@@ -1,5 +1,5 @@
-var password = 'APPLY'
-password=prompt('Welcome to Statistics Sierra Leone Recruitment Portal, Press APPLY to proceed ','APPLY');
-if (password != 'APPLY') {
+var password = 'OK'
+password=prompt('Welcome to Statistics Sierra Leone Recruitment Portal, Press OK to proceed ','OK');
+if (password != 'OK') {
 location.href='https://etteifot.github.io/StATs-SL-Recruitment';
 }
